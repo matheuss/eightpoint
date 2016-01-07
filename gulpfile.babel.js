fab30b0487171fdaf32679d027eb6da59c03d165
CodeTheory <hello@timothy.is>
2016-01-07 22:15:13 +0100

    Add comments to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,17 +15,26 @@ const CSS_GLOB = `${SRC_DIR}/**/*.css`;
 const CSS_PARTIALS = `!${SRC_DIR}/**/_*.css`;
 const JS_GLOB = `${SRC_DIR}/**/*.js`;
 
+
+// Task to clean the build directory
 export const clean = () => del([BUILD_DIR])
 
+
+// Build CSS files
 export const css = () => src([CSS_GLOB, CSS_PARTIALS], { base: SRC_DIR })
   .pipe(dest(BUILD_DIR))
   .pipe(postcss([minify]))
   .pipe(rename({suffix: '.min'}))
   .pipe(dest(BUILD_DIR));
 
+
+// Watch Task
 export const watchSrc = () => watch(CSS_GLOB, css);
 
+
+// Task sequences
 export const dev = series(clean, css, watchSrc);
 export const dist = series(clean, css);
 
+// Default task export
 export default dev;
