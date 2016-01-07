e6125c8de39505a6e562870c71b4ec439a68b5ea
CodeTheory <hello@timothy.is>
2016-01-08 00:46:45 +0100

    Add some base reset styles to test postCSS and add some postcss modules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import { src, dest, watch, parallel, series } from 'gulp';
 import del from 'del';
 import postcss from 'gulp-postcss';
+import cssnext from 'postcss-cssnext';
 import autoprefixer from 'autoprefixer';
 import mqpacker from 'css-mqpacker';
 import minify from 'cssnano';
+import simpleVars from 'postcss-simple-vars';
 import rename from 'gulp-rename';
 
 // Directories
@@ -22,6 +24,7 @@ export const clean = () => del([BUILD_DIR])
 
 // Build CSS files
 export const css = () => src([CSS_GLOB, CSS_PARTIALS], { base: SRC_DIR })
+  .pipe(postcss([autoprefixer, cssnext, simpleVars]))
   .pipe(dest(BUILD_DIR))
   .pipe(postcss([minify]))
   .pipe(rename({suffix: '.min'}))
